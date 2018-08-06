e1356e554d7f3d13665c45a113796a699fd03659
yunussim <yunussimulya@gmail.com>
2018-08-06 10:32:04 +0700

    Update ViewModel - Film (add load more sample)

--- app/src/main/java/com/gmail/yunussimulya/ghibli/viewmodel/FilmViewModel.java
+++ app/src/main/java/com/gmail/yunussimulya/ghibli/viewmodel/FilmViewModel.java
@@ -38,6 +38,30 @@ public class FilmViewModel extends ViewModel {
         });
     }
 
+    public void loadMoreFilms() {
+        repository.getFilms().enqueue(new Callback<List<Film>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<Film>> call, @NonNull Response<List<Film>> response) {
+                if (response.isSuccessful() && response.code() == 200) {
+                    List<Film> oldValue = films.getValue();
+                    if (oldValue != null && response.body() != null) oldValue.addAll(response.body());
+                    films.postValue(oldValue);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<Film>> call, @NonNull Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onCleared() {
+        if (!repository.getFilms().isCanceled()) repository.getFilms().cancel();
+        super.onCleared();
+    }
+
     public MutableLiveData<List<Film>> getFilms() {
         return films;
     }
