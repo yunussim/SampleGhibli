e512850928f9fa02b664dcf80cd47147dd5e9d2d
yunussim <yunussimulya@gmail.com>
2018-11-22 17:40:18 +0700

    Add log interceptor on retrofit

--- app/src/main/java/com/gmail/yunussimulya/ghibli/api/ApiClient.java
+++ app/src/main/java/com/gmail/yunussimulya/ghibli/api/ApiClient.java
@@ -1,20 +1,39 @@
 package com.gmail.yunussimulya.ghibli.api;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
+
+    private static final String BASE_URL = "https://ghibliapi.herokuapp.com/";
 
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("https://ghibliapi.herokuapp.com/")
+                    .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(getOkHttpClient())
                     .build();
         }
         return retrofit;
+    }
+
+    private static OkHttpClient getOkHttpClient() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+        clientBuilder.readTimeout(60, TimeUnit.SECONDS);
+        clientBuilder.connectTimeout(60, TimeUnit.SECONDS);
+        clientBuilder.writeTimeout(60, TimeUnit.SECONDS);
+        clientBuilder.addInterceptor(loggingInterceptor);
+        return clientBuilder.build();
     }
 
 }
